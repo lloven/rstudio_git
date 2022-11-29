60aea3320986b6324a0cd7ff9303937bd32d5adc
Lauri Loven <lauri.loven@oulu.fi>
2022-11-29 16:50:51 +0200

    Add new scatter plot.

--- R/functions.R
+++ R/functions.R
@@ -16,6 +16,13 @@ scatter_penguins <- function(penguins_data) {
     scale_color_manual(values = c("darkorange","darkorchid","cyan4"))
 }
 
+scatter2_penguins <- function(penguins_data) {
+  ggplot(data = penguins_data, aes(x = bill_length_mm, y =bill_depth_mm)) +
+    geom_point(aes(color = species, 
+                   shape = species),
+               size = 2)  +
+    scale_color_manual(values = c("darkorange","darkorchid","cyan4"))
+}
 
 hist_penguins <- function(penguins_data) {
   ggplot(data = penguins_data, aes(x = flipper_length_mm)) +
